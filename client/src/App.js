1471fbf7a8a35ae2ddc93165301b372470d6a52e
Taarik Ashenafi <taarik@Taariks-MacBook-Pro.local>
2024-09-19 00:11:57 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            {/* Your code goes here */}
+        </div>
+    );
+}
+
+export default App;
